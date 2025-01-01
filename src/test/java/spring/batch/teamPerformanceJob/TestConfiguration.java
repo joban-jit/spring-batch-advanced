3c610576ea5a2ed51787b75841ca11c37a5c34ad
Jobanjit Singh <joban@192.168.1.11>
2025-01-01 11:02:36 +0530

    fixed test case

--- src/test/java/spring/batch/teamPerformanceJob/TestConfiguration.java
+++ src/test/java/spring/batch/teamPerformanceJob/TestConfiguration.java
@@ -1,8 +1,10 @@
 package spring.batch.teamPerformanceJob;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -12,7 +14,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
-
+@PropertySource("classpath:application.properties")
 public class TestConfiguration {
 
     @Bean
@@ -23,20 +25,20 @@ public class TestConfiguration {
     }
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(
+            @Value("${spring.datasource.driverClassName}") String driverClassName,
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password
+    ){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        // Set the JDBC URL for H2 (in-memory database)
-        dataSource.setUrl("jdbc:h2:mem:testdb");
-
-        // Set the H2 JDBC driver class name
-        dataSource.setDriverClassName("org.h2.Driver");
-
-        // Set the username and password for H2 (you can leave these as default for in-memory)
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-
+        dataSource
+                .setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
         return dataSource;
     }
+
 }
 
